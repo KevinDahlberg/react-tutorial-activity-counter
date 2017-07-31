6e1d3554015da100528a0d467b155efd6fa21f0b
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-07-30 23:00:59 -0500

    changed component to stateless

--- src/components/CodingDayCount.js
+++ src/components/CodingDayCount.js
@@ -1,32 +1,29 @@
-import React from 'react'
 import '../stylesheets/ui.scss'
 
-export const CodingDayCount = React.createClass({
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + '%')
-  },
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  },
-  render() {
-    return (
-      <div className="coding-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span>days</span>
-        </div>
-        <div className="home-days">
-          <span>{this.props.home}</span>
-          <span>days</span>
-        </div>
-        <div className="out-days">
-          <span>{this.props.out}</span>
-          <span>days</span>
-        </div>
-        <div>
-          <span>{this.calcGoalProgress(this.props.total, this.props.goal)}</span>
-        </div>
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%');
+};
+
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total/goal);
+};
+
+export const CodingDayCount = ({total, home, out, goal}) => (
+    <div className="coding-day-count">
+      <div className="total-days">
+        <span>{total}</span>
+        <span>days</span>
       </div>
-    )
-  }
-})
+      <div className="home-days">
+        <span>{home}</span>
+        <span>days</span>
+      </div>
+      <div className="out-days">
+        <span>{out}</span>
+        <span>days</span>
+      </div>
+      <div>
+        <span>{calcGoalProgress(total, goal)}</span>
+      </div>
+    </div>
+)
